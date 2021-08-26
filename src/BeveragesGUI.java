74c01d8add2d6a6c6593a6cf37c7093083cfdfe2
Nonhlahla <nhlaa.nh@gmail.com>
2021-08-27 00:10:24 +0200

    First commit

---
+++ src/BeveragesGUI.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class BeveragesGUI extends JFrame{
+    private JPanel mainPanel;
+    private JTextField a1003TextField;
+    private JTextField r24TextField;
+    private JButton saveButton1;
+    private JComboBox comboBox1;
+    private JComboBox comboBox4;
+    private JTextField r4999TextField;
+
+    public BeveragesGUI()
+    {
+        add(mainPanel);
+        setTitle("Beverages");
+        setSize(400,500);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+       BeveragesGUI beveragesGUI = new BeveragesGUI();
+       beveragesGUI.setVisible(true);
+    }
+}
